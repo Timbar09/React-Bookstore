5ce5c62e8ace40d8e883cd5d23ca8af5b078627d
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-02-25 00:21:19 +0200

    Add bookstore array

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,9 +1,19 @@
-const Book = () => (
+import PropTypes from 'prop-types';
+
+const Book = ({ bookProp }) => (
   <li style={{ display: 'flex', gap: '1rem' }}>
-    <span>The Secret of Life</span>
-    <span>Great Van Author</span>
-    <button type="button">Delete</button>
+    <span>{bookProp.title}</span>
+    <span>{bookProp.author}</span>
+    <input type="button" value="Delete" />
   </li>
 );
+
+Book.propTypes = {
+  bookProp: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Book;
