c55066fae5c72fcb194bd9a4bf90de5dc839aee8
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-02-28 09:33:50 +0200

    Link reducers and actions to categories

--- src/routes/Categories.jsx
+++ src/routes/Categories.jsx
@@ -1,7 +1,16 @@
-const Categories = () => (
-  <main>
-    <input type="button" value="Check status" />
-  </main>
-);
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categoriesSlice';
+
+const Categories = () => {
+  const { status } = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  return (
+    <main>
+      <h1>{status}</h1>
+      <input type="button" value="Check status" onClick={() => dispatch(checkStatus())} />
+    </main>
+  );
+};
 
 export default Categories;
