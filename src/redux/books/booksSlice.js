3951da2b2b448a3c4290968e0ad97dbfa0851b9c
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-03-01 14:26:11 +0200

    Add extrareducers

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getBooks, storeBook, deleteBook } from '../../api';
 
 const initialState = {
   books: [
@@ -21,36 +22,73 @@ const initialState = {
     //   category: 'Nonfiction',
     // },
   ],
+  isLoading: false,
 };
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  const response = await getBooks();
+  return response;
+});
+
+export const addBook = createAsyncThunk('books/addBook', async (book) => {
+  const response = await storeBook(book);
+  return response;
+});
+
+export const removeBook = createAsyncThunk('books/removeBook', async (id) => {
+  await deleteBook(id);
+  return id;
+});
 
 export const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook: (state, action) => {
-      const newState = { ...state };
-      const { payload } = action;
-      const newBook = {
-        item_id: payload.bookId,
-        title: payload.title,
-        author: payload.author,
-        cartegory: 'Nonfiction',
-      };
-      newState.books = [...newState.books, newBook];
-
-      return newState;
-    },
-    removeBook: (state, action) => {
-      const newState = { ...state };
-      const targetBookId = action.payload;
-
-      newState.books = newState.books.filter((book) => book.item_id !== targetBookId);
-
-      return newState;
-    },
+    // addBook: (state, action) => {
+    //   const newState = { ...state };
+    //   const { payload } = action;
+    //   const newBook = {
+    //     item_id: payload.bookId,
+    //     title: payload.title,
+    //     author: payload.author,
+    //     cartegory: 'Nonfiction',
+    //   };
+    //   newState.books = [...newState.books, newBook];
+    //   return newState;
+    // },
+    // removeBook: (state, action) => {
+    //   const newState = { ...state };
+    //   const targetBookId = action.payload;
+    //   newState.books = newState.books.filter((book) => book.item_id !== targetBookId);
+    //   return newState;
+    // },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        const newState = { ...state };
+        newState.isLoading = true;
+        return newState;
+      })
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        const newState = { ...state };
+        newState.isLoading = false;
+        newState.books = action.payload;
+        return newState;
+      })
+      .addCase(addBook.fulfilled, (state, action) => {
+        const newState = { ...state };
+        newState.books.push(action.payload);
+        return newState;
+      })
+      .addCase(removeBook.fulfilled, (state, action) => {
+        const newState = { ...state };
+        newState.books = newState.books.filter((book) => book.item_id !== action.payload);
+        return newState;
+      });
   },
 });
 
-export const { addBook, removeBook } = booksSlice.actions;
+// export const { addBook, removeBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
